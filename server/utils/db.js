c2ef44d99544c4fcaeffacadb9c239de57357861
Raj Paswan <raj.paswan@oracle.com>
2019-02-05 00:43:31 +0530

    fix db connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -5,12 +5,26 @@ const DB_PORT = process.env.DB_PORT;
 const DB_COLLECTION = process.env.DB_COLLECTION;
 
 const MONGO_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_COLLECTION}`;
+const OPTIONS = {
+    useNewUrlParser: true,
+    autoReconnect: true,
+    reconnectInterval: 5000,
+    reconnectTries: 60,
+    connectTimeoutMS: 10000
+};
 
-mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true
-}, function (err) {
-    if (err)
-        console.log('[db]', err);
-    else
-        console.log('[db] connected to', MONGO_URL);
+mongoose.connection.on('connected', function () {
+    console.log('[db] connected to', MONGO_URL);
 });
+
+mongoose.connection.on('error', function (err) {
+    console.log('[db] could not connect to', MONGO_URL);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('[db] disconnected to', MONGO_URL);
+});
+
+setTimeout(() => {
+    mongoose.connect(MONGO_URL, OPTIONS);
+}, 10000);
